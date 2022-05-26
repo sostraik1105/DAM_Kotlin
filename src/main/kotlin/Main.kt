679eb8863c106851a560e687467b409108582d5d
Daniel Puchuri <focusdani1105@gmail.com>
2022-05-26 17:56:24 -0500

    otros tipos de datos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,64 +1,25 @@
-import kotlin.reflect.typeOf
-
 fun main(){
-    /*
-    tipos de dato
-        - Boolean -> size = 1bit -> false/true
-        - Char -> size = 16bit -> 'a' to 'z', '/n' '/101'
-        - String -> "Hola"
-        - Array of int [1,2,3]
-        -----------------------------------------
-        NUMBERS
-        - Byte -> size = 8bit -> -127 to 128
-        - Double -> size = 64bit -> 3.431231231234 (max 16 decimales)
-        - Float -> size = 32bit -> 3.4123232 (max 7 decimales)
-        - Int -> size = 32bit -> -2 -1 0 1 2
-        - Long -> size = 64bit -> -2L -1L 0L 1L 2L
-        - Short -> size = 16bit -> none
-    */
+    // Booleanos
+    val verdadero: Boolean = true
+    val falso: Boolean = false
+    val nulo: Boolean? = null
 
-    val one = 1 // int
-    println(one);
+    // Operadores Lógicos (OL)
+    println(verdadero || falso) // "||" representa el OL 'OR' -> true
+    println(verdadero && falso) // "&&" representa el OL 'AND' -> false
+    println(!verdadero) // "!" representa el OL 'NOT' -> false
 
-    val pi = 3.14 // double
-    println(pi)
+    // Arrays
+    var arr = arrayOf(1,"Ursula",false);
+    println(arr)
 
-    val num1Double = 12.32 // Double
-    println(num1Double::class.simpleName) // ::class.simpleName -> para ver el tipo de dato
+    // existe tipos primitivos de arrays
+    var byteArr: ByteArray = byteArrayOf(1, -2) // solo admite bytes
+    println(byteArr)
 
-    // en el caso de floats, debemos agregarle una f o F al final
-    val num1Float = 12.32f // Float
-    println(num1Float::class.simpleName)
+    var shortArr: ShortArray = shortArrayOf(150, -200) // solo admite shorts
+    println(shortArr)
 
-    // tambien podemos ser mós especificos y decirle el tipo de dato (recomendable)
-    val two: Int = 2
-    println(two)
-
-    // val three: Double = 3 -> devuelve error, los Double deben tener tener almenos un decimal
-    val three: Double = 3.0
-    println(three)
-
-    // cuando un float se pasa de los 7 decimales, se redondea automaticamente
-    val e: Float = 2.7182818284f
-    println(e) // -> 2.7182817
-
-
-    /*
-        Conversiones entre tipos de datos (explicito)
-            [siendo 'el' un tipo de dato]
-            - el.toByte() -> Byte
-            - el.toShort() -> Short
-            - el.toInt() -> Int
-            - el.toLong() -> Long
-            - el.toFloat() -> Float
-            - el.toDouble() -> Double
-            - el.toChar() -> Char
-            - el.toString() -> String
-    */
-
-    val x: Byte = 1
-    // val y: Int = x -> ERRROR!!! recuerda que x es un Byte pero y es un Int, entonces...
-    val y: Int = x.toInt();
-    println(y)
-
+    var intArr: IntArray = intArrayOf(100000, -21597523) // solo admite ints
+    println(intArr)
 }
